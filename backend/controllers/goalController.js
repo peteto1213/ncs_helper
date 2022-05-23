a3ae159c419d60c63de90661619beabfa70613cf
peteto1213 <peteto1213@gmail.com>
2022-05-23 18:10:00 +0100

    added CRUD rest API for goal components

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require('express-async-handler')
+const Goal = require('../models/goalModel') //Goal model created in the model folder
 
 /**
  * @author Pete To
@@ -7,7 +8,10 @@ const asyncHandler = require('express-async-handler')
  * @access Private
  */
 const getGoals = asyncHandler( async (req,res) => {
-    res.status(200).json({message: 'Get goals'})
+    // Get all goals from the db
+    const goals = await Goal.find()
+
+    res.status(200).json(goals)
 })
 
 /**
@@ -21,7 +25,11 @@ const setGoal = asyncHandler( async (req,res) => {
         res.status(400)
         throw new Error('Please add a text field')
     }
-    res.status(200).json({message: 'Post goals'})
+
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+    res.status(200).json(goal)
 })
 
 /**
@@ -31,7 +39,21 @@ const setGoal = asyncHandler( async (req,res) => {
  * @access Private
  */
 const updateGoal = asyncHandler( async (req,res) => {
-    res.status(200).json({message: `Update goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    const updatedGoal = await Goal.findByIdAndUpdate(
+        req.params.id, 
+        req.body, 
+        {
+            new: true
+        }
+    )
+
+    res.status(200).json(updateGoal)
 })
 
 /**
@@ -41,7 +63,13 @@ const updateGoal = asyncHandler( async (req,res) => {
  * @access Private
  */
 const deleteGoal = asyncHandler( async (req,res) => {
-    res.status(200).json({message: `Delete goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    await goal.remove()
+    res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
