5d39820d905d27e13ee15a741a4c98fd2ea9fc90
peteto1213 <peteto1213@gmail.com>
2022-05-26 22:34:42 +0100

    Rest API fundametal set up

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log('Hello');
+const express = require('express')
+const dotenv = require('dotenv').config //Allow access to the environmental variable in ".env"
+
+const port = process.env.PORT || 5000
+
+const app = express()
+
+//Get the body of the request
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+//first route - studyPlan
+app.use('/api/studyPlan', require('./routes/studyPlanRoutes'))
+
+app.listen(port, () => console.log(`Server running at port ${port}`))
