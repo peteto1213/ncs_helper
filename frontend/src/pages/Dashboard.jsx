b083d5e6d32f7b751407938d5a35bbec177cacda
peteto1213 <peteto1213@gmail.com>
2022-05-29 22:58:26 +0100

    created initial design for dashboard page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {FaRocket} from 'react-icons/fa'
+import collaboration from '../resources/collaboration.png'
+import blog from '../resources/blog.jpg'
+import feedback from '../resources/feedback.jpg'
 
 function Dashboard() {
   return (
-    <section>
-      <h1 className='heading'>Dashboard</h1>
+    <section className='dashboard'>
+      <div className='heading'>
+        <FaRocket /> 
+        <h1>Dashboard</h1>
+      </div>
+
+      <div className="card-container">
+
+        <div className="card">
+          <img src={collaboration} alt="collaboration" />
+          <h3>Collaboration Guides</h3>
+          <p>Manage or publish your created guides according to different courses in the program to help others!</p>
+          <Link to='/collaboration'>Manage</Link>
+        </div>
+
+        <div className="card">
+          <img src={blog} alt="blog" />
+          <h3>Blogs</h3>
+          <p>Manage your personal blogs or write a blog for leisure and sharing purposes!</p>
+          <Link to='/blog'>View</Link>
+        </div>
+
+        <div className="card">
+          <img src={feedback} alt="feedback" />
+          <h3>Feedback</h3>
+          <p>Tell us how you feel about this web application!</p>
+          <Link to='/feedback'>Send Feedback</Link>
+        </div>
+
+      </div>
+
     </section>
   )
 }
